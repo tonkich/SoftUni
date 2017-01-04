4e8145c917ef10c7b396e555c42c3dd5db9c490a
vrabeca <tonko1337@abv.bg>
2017-01-05 00:07:23 +0200

    renamings

---
+++ MSSQL-EF/Book shop system/BookShopSytem.Models/DTOs/AuthorDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSytem.Models.DTOs
+{
+    class AuthorDTO
+    {
+    }
+}
